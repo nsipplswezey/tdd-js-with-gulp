c0bdfc13419d3f957711203a0cd907438ed0992d
Nick S <nsipplswezey@gmail.com>
2015-05-30 14:45:50 -0700

    Gulp automated testing in mocha.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,31 @@
 //Here's where the magic happens
-var config = require('./gulp.config')();
 var gulp = require('gulp');
+var inject = require('gulp-inject');
+var bowerFiles = require('main-bower-files');
+var mochaPhantomJS = require('gulp-mocha-phantomjs');
+
+gulp.task('injectIntoIndex', function() {
+  return gulp.src('test/index.html')
+
+      .pipe(inject(
+        gulp.src(bowerFiles({includeDev: true}), {read: false}),
+        {name: 'bower', relative: true}
+      ))
+      .pipe(inject(
+        gulp.src(['src/js/**/*.js'], {read: false}),
+        {name: 'all', relative: true}
+      ))
+
+      .pipe(inject(
+        gulp.src(['test/suites/**/*.js'], {read: false}),
+        {name: 'suites', relative: true}
+      ))
+      .pipe(gulp.dest('test'));
+
+});
+
+gulp.task('test', ['injectIntoIndex'], function () {
+  return gulp.src('test/index.html', {read: false})
+    .pipe(mochaPhantomJS());
+});
 
