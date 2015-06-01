2ffdc1bf86f3e560084ffd447cb0944ab8a0264b
Nick S <nsipplswezey@gmail.com>
2015-05-31 23:58:08 -0700

    Partially adds error handling. What a rabit hole.

--- src/js/selfBalancingBinaryTree.js
+++ src/js/selfBalancingBinaryTree.js
@@ -23,14 +23,63 @@ BinarySearchTree.prototype.rebalance = function(){
 //And it's not gonna be arbitrary made up objects
 //Let's mock up a listItem?
 
+//And now we're cooking
+//The listview has the following methods
+
+//new ListView($el [, options])... this would be instantiating a new binaryTree
+
+//append($el | listItem)... this is like addNode
+//Each node is either a jQuery element or a ListItem(wrapped jQuery element)
+
+//find(selector | $el)... this is like contains... except it returns the node
+//there are some interesting behind-the-scenes things done here
+
+//remove()... removes the node from the DOM (and tree) and cleans up after? (whats cleanup)
+
+//cleanup()... unbinds all events, but doesn't remove from dom.
+
+//so adding nodes is done behind the scenes on an append.
+//and nodes are either jQuery or listItems
+
+//honestly it doesn't matter that we use a number for value
+//its going to be an object
+
+//and what do we need to do with the binary search tree?
+//a find operation on an array is linnear time at worst... and then  you have to shift all other values
+//a find on a binary tree is logrithmic at worst
+
+//a delete operation on an array is... what time complexity?
+//a delete operation on a binary search tree is... what time complexity?
+
+
+//TODO: Build a better check for whether a node already exists
+
 BinarySearchTree.prototype.addNode = function(listItem){
 
+  var newNode = {
+    value : listItem,
+    left : null,
+    right : null
+  }
+
   if(this._root === null) {
-    this._root = listItem;
-  } else {
+    this._root = newNode;
+
+  } else if (listItem.id > this._root.value.id && this._root.right.value.id !== listItem.id) {
+    console.log("right");
     //traverse the tree
     //figure out where things go
+    this._root.right = newNode;
 
+  } else if (listItem.id < this._root.value.id && this._root.left.value.id !== listItem.id) {
+    console.log("left");
+    console.log(listItem.id);
+    console.log(this._root.value.id);
+
+    this._root.left = newNode;
+  } else {
+
+    throw new Error("A node with that id already exists in this tree");
   }
 
 }
--- test/suites/test.js
+++ test/suites/test.js
@@ -33,16 +33,58 @@ describe('Binary Search Tree', function(){
 
       assert.equal(bTree._root, null);
 
-      bTree.addNode({name:'list Item 1',
-                     id: 1});
+      bTree.addNode({name : 'list Item 1',
+                     id : 1}
+                   );
 
-      assert.equal(bTree._root.id, 1)
+      assert.equal(bTree._root.value.id, 1);
 
+    })
+
+    it('should add an item with an id larger than the root id to the right property of the root node',function(){
+
+      bTree.addNode({name : 'list Item 2',
+                     id : 2});
+
+      assert.equal(bTree._root.right.value.id, 2);
+
+    })
+
+    it('should add an item with an id smaller than the root id to the left property of the root node',function(){
+
+      bTree.addNode({name : 'list Item 3',
+                     id : 0});
+
+      assert.equal(bTree._root.left.value.id, 0);
 
 
     })
 
-  });
+    it('should throw an exception when a node with an id that already exists is added', function(){
+
+      function boom(blowup){
+        if(blowup)
+          throw new Error('BOOM');
+      }
+
+      assert.throws( function() { boom(true); },
+                    Error );
+
+      assert.throws( function() { bTree.addNode({name : 'list Item 3',
+                                               id : 0 }) },
+                    Error)
+
+
+  })
+
+  it('should be able to add an arbitrary number of nodes to the correct locations', function(){
+
+
+
+  })
+
+
+});
 
 });
 
